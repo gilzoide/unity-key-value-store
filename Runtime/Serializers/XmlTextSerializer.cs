a2b3e859ad4705b541f54294f6d4ea534a4e35d4
gilzoide <gilzoide@gmail.com>
2023-07-10 18:24:28 -0300

    Add XML serializer implementation

---
+++ Runtime/Serializers/XmlTextSerializer.cs
@@ -0,0 +1,25 @@
+using System.IO;
+using System.Xml.Serialization;
+
+namespace Gilzoide.KeyValueStore.Serializers
+{
+    public class XmlTextSerializer : ITextSerializer
+    {
+        public T DeserializeObject<T>(string text)
+        {
+            using (var reader = new StringReader(text))
+            {
+                return (T) (new XmlSerializer(typeof(T)).Deserialize(reader));
+            }
+        }
+
+        public string SerializeObject<T>(T obj)
+        {
+            using (var writer = new StringWriter())
+            {
+                new XmlSerializer(typeof(T)).Serialize(writer, obj);
+                return writer.ToString();
+            }
+        }
+    }
+}
