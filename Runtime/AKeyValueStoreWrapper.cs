cc502feb21bb1e48acb0837bdc7123dc374dc0ad
gilzoide <gilzoide@gmail.com>
2023-07-13 18:39:40 -0300

    Add AKeyValueStoreWrapper for generalizing store wrapper classes

---
+++ Runtime/AKeyValueStoreWrapper.cs
@@ -0,0 +1,99 @@
+using System;
+
+namespace Gilzoide.KeyValueStore
+{
+    public abstract class AKeyValueStoreWrapper<TKVS> : IKeyValueStore
+        where TKVS : IKeyValueStore
+    {
+        public TKVS WrappedKeyValueStore { get; }
+
+        public AKeyValueStoreWrapper(TKVS kvs)
+        {
+            if (kvs == null)
+            {
+                throw new ArgumentNullException(nameof(WrappedKeyValueStore));
+            }
+            WrappedKeyValueStore = kvs;
+        }
+
+        public virtual bool HasKey(string key)
+        {
+            return WrappedKeyValueStore.HasKey(key);
+        }
+
+        public virtual void DeleteKey(string key)
+        {
+            WrappedKeyValueStore.DeleteKey(key);
+        }
+
+        public virtual bool TryGetBool(string key, out bool value)
+        {
+            return WrappedKeyValueStore.TryGetBool(key, out value);
+        }
+
+        public virtual bool TryGetBytes(string key, out byte[] value)
+        {
+            return WrappedKeyValueStore.TryGetBytes(key, out value);
+        }
+
+        public virtual bool TryGetDouble(string key, out double value)
+        {
+            return WrappedKeyValueStore.TryGetDouble(key, out value);
+        }
+
+        public virtual bool TryGetFloat(string key, out float value)
+        {
+            return WrappedKeyValueStore.TryGetFloat(key, out value);
+        }
+
+        public virtual bool TryGetInt(string key, out int value)
+        {
+            return WrappedKeyValueStore.TryGetInt(key, out value);
+        }
+
+        public virtual bool TryGetLong(string key, out long value)
+        {
+            return WrappedKeyValueStore.TryGetLong(key, out value);
+        }
+
+        public virtual bool TryGetString(string key, out string value)
+        {
+            return WrappedKeyValueStore.TryGetString(key, out value);
+        }
+
+        public virtual void SetBool(string key, bool value)
+        {
+            WrappedKeyValueStore.SetBool(key, value);
+        }
+
+        public virtual void SetInt(string key, int value)
+        {
+            WrappedKeyValueStore.SetInt(key, value);
+        }
+
+        public virtual void SetLong(string key, long value)
+        {
+            WrappedKeyValueStore.SetLong(key, value);
+        }
+
+        public virtual void SetFloat(string key, float value)
+        {
+            WrappedKeyValueStore.SetFloat(key, value);
+        }
+
+        public virtual void SetDouble(string key, double value)
+        {
+            WrappedKeyValueStore.SetDouble(key, value);
+        }
+
+        public virtual void SetString(string key, string value)
+        {
+            WrappedKeyValueStore.SetString(key, value);
+        }
+
+        public virtual void SetBytes(string key, byte[] value)
+        {
+            WrappedKeyValueStore.SetBytes(key, value);
+        }
+    }
+}
--- Runtime/AutoSaveKeyValueStore.cs
+++ Runtime/AutoSaveKeyValueStore.cs
@@ -1,108 +1,60 @@
-using System;
 using System.Threading.Tasks;
 
 namespace Gilzoide.KeyValueStore
 {
-    public class AutoSaveKeyValueStore : ISavableKeyValueStore
+    public class AutoSaveKeyValueStore : AKeyValueStoreWrapper<ISavableKeyValueStore>, ISavableKeyValueStore
     {
-        public ISavableKeyValueStore WrappedKeyValueStore { get; }
-
         private bool isSaveScheduled = false;
 
-        public AutoSaveKeyValueStore(ISavableKeyValueStore kvs)
+        public AutoSaveKeyValueStore(ISavableKeyValueStore kvs) : base(kvs)
         {
-            if (kvs == null)
-            {
-                throw new ArgumentNullException(nameof(WrappedKeyValueStore));
-            }
-            WrappedKeyValueStore = kvs;
         }
 
-        public bool HasKey(string key)
+        public override void DeleteKey(string key)
         {
-            return WrappedKeyValueStore.HasKey(key);
-        }
-
-        public void DeleteKey(string key)
-        {
-            WrappedKeyValueStore.DeleteKey(key);
+            base.DeleteKey(key);
             Save();
         }
 
-        public bool TryGetBool(string key, out bool value)
+        public override void SetBool(string key, bool value)
         {
-            return WrappedKeyValueStore.TryGetBool(key, out value);
-        }
-
-        public bool TryGetBytes(string key, out byte[] value)
-        {
-            return WrappedKeyValueStore.TryGetBytes(key, out value);
-        }
-
-        public bool TryGetDouble(string key, out double value)
-        {
-            return WrappedKeyValueStore.TryGetDouble(key, out value);
-        }
-
-        public bool TryGetFloat(string key, out float value)
-        {
-            return WrappedKeyValueStore.TryGetFloat(key, out value);
-        }
-
-        public bool TryGetInt(string key, out int value)
-        {
-            return WrappedKeyValueStore.TryGetInt(key, out value);
-        }
-
-        public bool TryGetLong(string key, out long value)
-        {
-            return WrappedKeyValueStore.TryGetLong(key, out value);
-        }
-
-        public bool TryGetString(string key, out string value)
-        {
-            return WrappedKeyValueStore.TryGetString(key, out value);
-        }
-
-        public void SetBool(string key, bool value)
-        {
-            WrappedKeyValueStore.SetBool(key, value);
+            base.SetBool(key, value);
             Save();
         }
 
-        public void SetInt(string key, int value)
+        public override void SetInt(string key, int value)
         {
-            WrappedKeyValueStore.SetInt(key, value);
+            base.SetInt(key, value);
             Save();
         }
 
-        public void SetLong(string key, long value)
+        public override void SetLong(string key, long value)
         {
-            WrappedKeyValueStore.SetLong(key, value);
+            base.SetLong(key, value);
             Save();
         }
 
-        public void SetFloat(string key, float value)
+        public override void SetFloat(string key, float value)
         {
-            WrappedKeyValueStore.SetFloat(key, value);
+            base.SetFloat(key, value);
             Save();
         }
 
-        public void SetDouble(string key, double value)
+        public override void SetDouble(string key, double value)
         {
-            WrappedKeyValueStore.SetDouble(key, value);
+            base.SetDouble(key, value);
             Save();
         }
 
-        public void SetString(string key, string value)
+        public override void SetString(string key, string value)
         {
-            WrappedKeyValueStore.SetString(key, value);
+            base.SetString(key, value);
             Save();
         }
 
-        public void SetBytes(string key, byte[] value)
+        public override void SetBytes(string key, byte[] value)
         {
-            WrappedKeyValueStore.SetBytes(key, value);
+            base.SetBytes(key, value);
             Save();
         }
 
